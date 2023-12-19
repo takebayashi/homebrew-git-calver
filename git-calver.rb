d5c8199658c56d3837eedc7ac64d38c71c96731e
Shun Takebayashi <shun@takebayashi.asia>
2023-12-19 09:01:08 +0900

    Update to 23.12.1

--- git-calver.rb
+++ git-calver.rb
@@ -1,9 +1,9 @@
 class GitCalver < Formula
   desc "carlendar versioning utility"
   homepage "https://github.com/takebayashi/git-calver"
-  url "https://github.com/takebayashi/git-calver/releases/download/20.3.3/git-calver-x86_64-apple-darwin.zip"
-  version "20.3.3"
-  sha256 "c61dee0d092dd4cb21f3955d4c291f05744ddfdfa88021df6853ed623d0a3980"
+  url "https://github.com/takebayashi/git-calver/releases/download/23.12.1/git-calver-x86_64-apple-darwin.zip"
+  version "23.12.1"
+  sha256 "972cf6b97647f9e57ceb4bb67bb30e6e874da7c3e8774fa27fc465ab5c6f355e"
 
   depends_on "openssl@1.1"
 
